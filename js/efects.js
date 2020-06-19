edd526e038eeb45fc65105927f56595c432d200c
Oscar Quintanilla <oscar.david.quintanilla@gmail.com>
2020-06-18 23:22:50 -0600

    Version Entregada

--- js/efects.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  alert("ready!");
-  $('#home').click(function () {
-
-    $('html, body').animate({
-      scrollTop: $("#div1").offset().top
-    }, 2000);
-  })
-});
